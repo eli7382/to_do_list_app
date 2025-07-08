7039ca3ca18a1cf63e350329445f7d3e49019191
eli7382 <eli.zaberchik@gmail.com>
2025-07-09 02:24:30 +0300

    Add light and dark themes

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,28 @@ void main() => runApp(const ToDoListApp());
 class ToDoListApp extends StatelessWidget {
   const ToDoListApp({Key? key}) : super(key: key);
 
+  ThemeData _buildLightTheme() {
+    return ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: Colors.blue,
+    );
+  }
+
+  ThemeData _buildDarkTheme() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primarySwatch: Colors.blue,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'To Do List',
-      home: HomePage(),
+      theme: _buildLightTheme(),
+      darkTheme: _buildDarkTheme(),
+      themeMode: ThemeMode.system,
+      home: const HomePage(),
     );
   }
 }
-
